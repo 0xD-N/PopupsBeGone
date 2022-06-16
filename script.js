5d23254e89e37785d39973e104e9db7600088a4f
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-06-15 23:46:43 -0500

    initial progress

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+let getTab = async function()
+{
+    let tabs = await chrome.tabs.query({active: true, currentWindow: true, highlighted: true})
+    
+    return tabs[0]
+    
+}
+
+chrome.tabs.onActivated.addListener(async function()
+{
+    let currentTab = await getTab()
+    chrome.scripting.executeScript({files: ["./inject.js"], target: {tabId: currentTab["id"]}})
+    
+})
