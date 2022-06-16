00baf28e1c6cf5b346b3eddabb316f966efa8c75
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-06-16 00:01:27 -0500

    comments

--- script.js
+++ script.js
@@ -2,16 +2,17 @@
 
 let getTab = async function()
 {
-    let tabs = await chrome.tabs.query({active: true, currentWindow: true, highlighted: true})
+    let tabs = await chrome.tabs.query({active: true, currentWindow: true, highlighted: true}) //https://developer.chrome.com/docs/extensions/reference/tabs/#method-query
     
     return tabs[0]
     
 }
 
-chrome.tabs.onActivated.addListener(async function()
+chrome.tabs.onActivated.addListener(async function() //https://developer.chrome.com/docs/extensions/reference/tabs/#event-onActivated
 {
     let currentTab = await getTab()
-    console.log("here")
-    chrome.scripting.executeScript({files: ["./inject.js"], target: {tabId: currentTab["id"]}})
-    
+
+    //output is shown in normal console (foreground)
+    chrome.scripting.executeScript({files: ["./inject.js"], target: {tabId: currentTab["id"]}}) //https://developer.chrome.com/docs/extensions/reference/scripting/#method-executeScript
+     
 })
